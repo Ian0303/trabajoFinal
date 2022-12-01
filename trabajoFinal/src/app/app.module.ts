1274e08aa5315664faada5719568c750ed4338ed
Ian0303 <110197575+Ian0303@users.noreply.github.com>
2022-11-30 21:59:43 -0300

    corrección

--- trabajoFinal/src/app/app.module.ts
+++ trabajoFinal/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
+import { RedesSComponent } from './components/redes-s/redes-s.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    LogoAPComponent
+    LogoAPComponent,
+    RedesSComponent
   ],
   imports: [
     BrowserModule,
