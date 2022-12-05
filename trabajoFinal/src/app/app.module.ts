dcdbfc867423f8f851bc9011907dcf3cc8baa8bf
Ian0303 <110197575+Ian0303@users.noreply.github.com>
2022-12-05 11:41:10 -0300

    components experiencia y educacion, (Bootstrap implementado)

--- trabajoFinal/src/app/app.module.ts
+++ trabajoFinal/src/app/app.module.ts
@@ -9,6 +9,8 @@ import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
 import { RedesSComponent } from './components/redes-s/redes-s.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
+import { ExperienciaComponent } from './components/experiencia/experiencia.component';
+import { EducacionComponent } from './components/educacion/educacion.component';
 
 @NgModule({
   declarations: [
@@ -17,7 +19,9 @@ import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
     LogoAPComponent,
     RedesSComponent,
     BannerComponent,
-    AcercaDeComponent
+    AcercaDeComponent,
+    ExperienciaComponent,
+    EducacionComponent
   ],
   imports: [
     BrowserModule,
