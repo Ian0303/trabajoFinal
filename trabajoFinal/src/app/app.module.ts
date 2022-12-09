349a1e34e3a4709e8212e909df53d1fcc246fe84
Ian0303 <110197575+Ian0303@users.noreply.github.com>
2022-12-09 08:25:08 -0300

    ultimos componentes

--- trabajoFinal/src/app/app.module.ts
+++ trabajoFinal/src/app/app.module.ts
@@ -3,10 +3,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './components/header/header.component';
 import { LogoAPComponent } from './components/logo-ap/logo-ap.component';
-import { RedesSComponent } from './components/redes-s/redes-s.component';
 import { BannerComponent } from './components/banner/banner.component';
 import { AcercaDeComponent } from './components/acerca-de/acerca-de.component';
 import { ExperienciaComponent } from './components/experiencia/experiencia.component';
@@ -21,7 +20,6 @@ import { FooterComponent } from './components/footer/footer.component';
     AppComponent,
     HeaderComponent,
     LogoAPComponent,
-    RedesSComponent,
     BannerComponent,
     AcercaDeComponent,
     ExperienciaComponent,
@@ -34,6 +32,7 @@ import { FooterComponent } from './components/footer/footer.component';
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    BrowserAnimationsModule,
     NgCircleProgressModule.forRoot({})
   ],
   providers: [],
