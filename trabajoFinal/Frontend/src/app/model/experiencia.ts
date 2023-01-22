ff872e457c992bba3839ce187194a8c11acc7f95
Ian0303 <110197575+Ian0303@users.noreply.github.com>
2023-01-22 00:13:22 -0300

    token y componente experiencia(edición)

---
+++ trabajoFinal/Frontend/src/app/model/experiencia.ts
@@ -0,0 +1,12 @@
+export class Experiencia {
+    id? : number;
+    nombreE : string;
+    descrpcionE : string;
+
+    constructor(nombreE: string, descripcionE: string){
+        this.nombreE = nombreE;
+        this.descrpcionE = descripcionE;
+    }
+
+
+}
