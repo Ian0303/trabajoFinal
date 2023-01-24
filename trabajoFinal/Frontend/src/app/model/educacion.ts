d0d4e499e85d28930b497d41a5f602db9830b0f1
Ian0303 <110197575+Ian0303@users.noreply.github.com>
2023-01-24 15:02:33 -0300

    componente educacion(fin)

---
+++ trabajoFinal/Frontend/src/app/model/educacion.ts
@@ -0,0 +1,10 @@
+export class Educacion {
+    id? : number;
+    nombreE : string;
+    descrpcionE : string;
+
+    constructor(nombreE: string, descripcionE: string){
+        this.nombreE = nombreE;
+        this.descrpcionE = descripcionE;
+    }
+}
--- trabajoFinal/Frontend/src/app/service/s-experiencia.service.ts
+++ trabajoFinal/Frontend/src/app/service/s-experiencia.service.ts
@@ -9,26 +9,25 @@ import { Experiencia } from '../model/experiencia';
 export class SExperienciaService {
   expURL = 'http://localhost:8080/explab/'
 
-
   constructor(private httpClient: HttpClient) { }
 
   public lista(): Observable<Experiencia[]> {
     return this.httpClient.get<Experiencia[]>(this.expURL + 'lista');
   }
 
-public detail(id: number): Observable<Experiencia>{
-  return this.httpClient.get<Experiencia>(this.expURL + `detail/${id}`);
-}
+  public detail(id: number): Observable<Experiencia> {
+    return this.httpClient.get<Experiencia>(this.expURL + `detail/${id}`);
+  }
 
-public save(experiencia: Experiencia): Observable<any>{
-  return this.httpClient.post<any>(this.expURL + 'create', experiencia);
-}
+  public save(experiencia: Experiencia): Observable<any> {
+    return this.httpClient.post<any>(this.expURL + 'create', experiencia);
+  }
 
-public update(id: number, experiencia: Experiencia): Observable<any>{
-  return this.httpClient.put<any>(this.expURL + `update/${id}`, experiencia);
-}
+  public update(id: number, experiencia: Experiencia): Observable<any> {
+    return this.httpClient.put<any>(this.expURL + `update/${id}`, experiencia);
+  }
 
-public delete(id: number): Observable<any>{
-  return this.httpClient.delete<any>(this.expURL + `delete/${id}`);
-}
+  public delete(id: number): Observable<any> {
+    return this.httpClient.delete<any>(this.expURL + `delete/${id}`);
+  }
 }
